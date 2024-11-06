8a9f56c823f46c50db1f07ad57d1be69ef6f74c1
Mel Mel Mom <melmelmomof4@gmail.com>
2024-11-05 22:55:51 -0500

    arrays

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test";
+console.log(secondCharacter);
+
+
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
