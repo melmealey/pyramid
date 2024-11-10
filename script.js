2647980ba19fbdf1420868c7d19b40211609a4c0
Mel Mel Mom <melmelmomof4@gmail.com>
2024-11-09 21:47:05 -0500

    learning for loop

--- script.js
+++ script.js
@@ -25,3 +25,9 @@ let popped = rows.pop();  // This removes and returns the last element, "freeCod
 // Log the popped element and the current state of the array
 console.log(popped);  // This will log "freeCodeCamp"
 console.log(rows);    // This will log the array after popping: ["Naomi", "Quincy", "CamperChan"]
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = ""
