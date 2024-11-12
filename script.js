2a86258cb924ccc3287e9cb05ab5b8d0d14c8c95
Mel Mel Mom <melmelmomof4@gmail.com>
2024-11-11 22:05:51 -0500

    .repeat

--- script.js
+++ script.js
@@ -30,4 +30,23 @@ for (let i = 0; i < count; i = i + 1) {
   rows.push(i);
 }
 
-let result = ""
+// let result = ""
+
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+
+
+
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(character.repeat(i + 1))
+// }
+
+// let result = ""
+
+// for (const row of rows) {
+//   result = result + row + "\n";
+// }
+
+// console.log(result);
