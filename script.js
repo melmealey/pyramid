7900c22be12ab1f64774c13e1bfa24c4b0b5b4c5
Mel Mel Mom <melmelmomof4@gmail.com>
2024-11-12 22:28:11 -0500

    finally passed #49

--- script.js
+++ script.js
@@ -36,7 +36,31 @@ for (let i = 0; i < count; i = i + 1) {
 // const count = 8;
 // const rows = [];
 
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(character.repeat(i + 1))
+// }
 
+// let result = ""
+
+// for (const row of rows) {
+//   result = result + row + "\n";
+// }
+
+// console.log(result);
+
+
+// finally figured out what they were asking me to do!!!!//
+
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// function padRow(name) {
+//   return name;
+// }
+
+// const call = padRow("CamperChan");
+// console.log(call);
 
 
 // for (let i = 0; i < count; i = i + 1) {
